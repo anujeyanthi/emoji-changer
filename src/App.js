4bdc5d99fc5fdb62208ed1d19ab489d0cc6f4df8
dshree-34 <dhanushreesenthilkumar2811@gmail.com>
2025-06-30 14:57:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
+import MoodDisplay from './MoodDisplay';
 
 function App() {
+  const[mood, setMood] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`container ${mood}`}>
+      <h1>How are you feeling today?</h1>
+      <div className="buttons">
+        <button className="btn happy" onClick={() => setMood("happy")}>😁happy</button>
+        <button className="btn sad" onClick={() => setMood("sad")}>😔sad</button>
+        <button className="btn excite" onClick={() => setMood("excite")}>😍excite</button>
+        <button className="btn angry" onClick={() => setMood("angry")}>😤angry</button>
+      </div>
+      
+      <MoodDisplay value={mood}/>
     </div>
   );
 }
